cb6767abfa8a9b35f5c976ec1ac7f38d8afd6be8
severinodoramo <160549283+severinodoramo@users.noreply.github.com>
2024-09-17 16:08:04 -0300

    first commit

--- controllers/usuario.py
+++ controllers/usuario.py
@@ -2,10 +2,14 @@ from flask import render_template, request, redirect, flash, url_for
 from utils import db, lm
 from models.usuario import Usuario
 from flask import Blueprint
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 import hashlib
 
 bp_usuario = Blueprint("usuario", __name__, template_folder='templates')
+
+@bp_usuario.route('/')
+def home():
+    return redirect(url_for('usuario.recovery'))
 
 @bp_usuario.route('/recovery')
 def recovery():
@@ -26,15 +30,22 @@ def create():
         md5.update(senha.encode('utf-8'))
         senha_cripto = md5.hexdigest()
 
-        email_existente = Usuario.query.filter_by(email=email).first()
-        if email_existente:
-            flash('Email existente!')
+        user = Usuario.query.filter_by(email=email).first()
+        if user:
+            flash('Email já exsite!', category='error')
+            return render_template('usuario_create.html')
+        elif len(nome) < 3:
+            flash('Nome muito pequeno!', category='error')
+            return render_template('usuario_create.html')
+        elif len(senha) < 8:
+            flash('Senha deve ter no mínimo 8 caracteres', category='error')
             return render_template('usuario_create.html')
         else:
             u = Usuario(nome, email, senha_cripto)
             db.session.add(u)
             db.session.commit()
             login_user(u)
+            flash('Conta criada com sucesso!', category='success')
             return redirect(url_for('usuario.recovery'))
       
 
@@ -55,6 +66,7 @@ def autenticar():
 
     if (usuario and usuario.senha == senha_cripto):
         login_user(usuario)
+        flash('Logado com suceso!')
         return redirect(url_for('usuario.recovery'))
     else:
         flash('Login ou senha incorretos')
@@ -65,3 +77,47 @@ def logoff():
     logout_user()
     flash('Usuário desconectado do sistema')
     return redirect('/login')
+
+@bp_usuario.route('/update', methods=['GET', 'POST'])
+def update():
+    id_usuario = current_user.id
+    usuario = Usuario.query.filter_by(id=id_usuario).first()
+
+    if request.method == 'GET':
+        return render_template('usuario_update.html', usuario=usuario)
+    
+    if request.method=="POST":
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        usuario_atual = Usuario.query.filter_by(id=current_user.id).first()
+        email_atual = usuario_atual.email
+
+        md5 = hashlib.md5()
+        md5.update(senha.encode('utf-8'))
+        senha_cripto = md5.hexdigest()
+
+        user = Usuario.query.filter_by(email=email).first()
+        if user and user.email != email_atual:
+            flash('Email já exsite!', category='error')
+            return redirect(url_for('usuario.update'))
+        elif len(nome) < 3:
+            flash('Nome muito pequeno!', category='error')
+            return redirect(url_for('usuario.update'))
+        elif senha_cripto != user.senha:
+            flash('Senha incorreta!', category='error')
+            return redirect(url_for('usuario.update'))
+        else:
+            user.nome = nome
+            user.email = email
+            db.session.commit()
+            flash('Conta atualizada com sucesso!', category='success')
+            return redirect(url_for('usuario.recovery'))
+        
+@bp_usuario.route('/delete')
+def delete():
+    usuario = Usuario.query.filter_by(id=id, id_usuario=current_user.id).first()
+    db.session.delete(usuario)
+    db.session.commit()
+    return render_template('login.html')
